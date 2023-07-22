ec8a916212f13f1aee98705ca5d54ceef9a6a36d
Frank Wang <frankningwang@gmail.com>
2023-07-22 14:44:37 -0700

    fix alignment and bkgd

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -19,57 +19,68 @@ function Home() {
   }
 
   return (
-    <Container
-      fluid
-      className="home-section h-100"
-      id="home">
-      <Container className="home-content">
-        <Row>
-          <Col
-            md={7}
-            className="home-header">
-            <h1
-              style={{ paddingBottom: 15 }}
-              className="heading">
-              Hi There!{" "}
-            </h1>
+    <>
+      <section>
+        <Container
+          fluid
+          className="home-section h-100"
+          id="home">
+          <Container className="home-content">
+            <Row>
+              <Col
+                md={7}
+                className="home-header">
+                <h1
+                  style={{ paddingBottom: 15 }}
+                  className="heading">
+                  Hi There!{" "}
+                </h1>
 
-            <h1 className="heading-name">
-              Welcome to:
-              <strong className="main-name"> TOSter</strong>
-            </h1>
+                <h1 className="heading-name">
+                  Welcome to:
+                  <strong className="main-name"> TOSter</strong>
+                </h1>
 
-            <div className="heading-navbar">
-              <Type />
-            </div>
-          </Col>
+                <div className="heading-navbar">
+                  <Type />
+                </div>
+              </Col>
 
-          {
-            <Col
-              md={5}
-              style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "400px" }}
-              />
-            </Col>
-          }
-        </Row>
-        <button
-          className="button-container"
-          onClick={handleUpload}>
-          Get Started
-        </button>
+              {
+                <Col
+                  md={5}
+                  style={{ paddingBottom: 20 }}>
+                  <img
+                    src={homeLogo}
+                    alt="home pic"
+                    className="img-fluid"
+                    style={{ maxHeight: "400px" }}
+                  />
+                </Col>
+              }
+            </Row>
+          </Container>
 
-        <button
-          className="button-container"
-          onClick={handleSearch}>
-          Search
-        </button>
-      </Container>
-    </Container>
+          <button
+            className="button-container-primary"
+            onClick={handleUpload}>
+            Get Started
+          </button>
+
+          <button
+            className="button-container-secondary"
+            onClick={handleSearch}>
+            Search
+          </button>
+
+          <button
+            className="button-container-secondary"
+            onClick={handleResults}>
+            Results
+          </button>
+        </Container>
+      </section>
+    </>
   );
 }
 
