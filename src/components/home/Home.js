71fdcf818dfe1f9af7523bde1295169a2db7060d
Laurence <laurenceliao123@gmail.com>
2023-07-21 20:28:02 -0700

    stuff

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,42 +5,39 @@ import Type from "./Type";
 
 function Home() {
   return (
-    // <section>
-    //   <Container fluid className="home-section" id="home">
-    //     <Particle />
-    //     <Container className="home-content">
-    //       <Row>
-    //         <Col md={7} className="home-header">
-    //           <h1 style={{ paddingBottom: 15 }} className="heading">
-    //             Hi There!{" "}
-    //           </h1>
+     <section>
+       <Container fluid className="home-section" id="home">
+         <Particle />
+         <Container className="home-content">
+           <Row>
+             <Col md={7} className="home-header">
+               <h1 style={{ paddingBottom: 15 }} className="heading">
+                 Hi There!{" "}
+               </h1>
 
-    //           <h1 className="heading-name">
-    //             Welcome to:
-    //             <strong className="main-name"> Hackathon Project</strong>
-    //           </h1>
+               <h1 className="heading-name">
+                 Welcome to:
+                 <strong className="main-name"> Hackathon Project</strong>
+               </h1>
 
-    //           <div style={{ padding: 50, textAlign: "left" }}>
-    //             <Type />
-    //           </div>
-    //         </Col>
+               <div style={{ padding: 50, textAlign: "left" }}>
+                 <Type />
+              </div>
+             </Col>
 
-    //         <Col md={5} style={{ paddingBottom: 20 }}>
-    //           {/* <img
-    //             src={homeLogo}
-    //             alt="home pic"
-    //             className="img-fluid"
-    //             style={{ maxHeight: "450px" }}
-    //           /> */}
-    //         </Col>
-    //       </Row>
-    //     </Container>
-    //   </Container>
-    // </section>
+             <Col md={5} style={{ paddingBottom: 20 }}>
+               {/* <img
+                 src={homeLogo}
+                 alt="home pic"
+                 className="img-fluid"
+                 style={{ maxHeight: "450px" }}
+               /> */}
+             </Col>
+          </Row>
+        </Container>
+       </Container>
+     </section>
 
-    <div>
-        Hi
-    </div>
   );
 }
 
