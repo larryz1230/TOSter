dc1192ecebdd2dd9cfa649d89bbd64154a8a1590
Frank Wang <frankningwang@gmail.com>
2023-07-22 09:21:08 -0700

    i have no idea what im doing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,74 +6,71 @@ import Type from "./Type";
 import homeLogo from "../../Assets/transparent-toast.png";
 
 function Home() {
-
   function handleUpload() {
-    window.location.href = 'http://localhost:3000/upload'
+    window.location.href = "http://localhost:3000/upload";
   }
 
   function handleSearch() {
-    window.location.href = 'http://localhost:3000/search'
+    window.location.href = "http://localhost:3000/search";
   }
 
   function handleResults() {
-    window.location.href = 'http://localhost:3000/results'
+    window.location.href = "http://localhost:3000/results";
   }
 
   return (
-    <>
-     <section>
-     <Container
-        fluid
-        className="home-section h-100"
-        id="home">
-        <Container className="home-content">
-          <Row>
+    <Container
+      fluid
+      className="home-section h-100"
+      id="home">
+      <Container className="home-content">
+        <Row>
+          <Col
+            md={7}
+            className="home-header">
+            <h1
+              style={{ paddingBottom: 15 }}
+              className="heading">
+              Hi There!{" "}
+            </h1>
+
+            <h1 className="heading-name">
+              Welcome to:
+              <strong className="main-name"> TOSter</strong>
+            </h1>
+
+            <div className="heading-navbar">
+              <Type />
+            </div>
+          </Col>
+
+          {
             <Col
-              md={7}
-              className="home-header">
-              <h1
-                style={{ paddingBottom: 15 }}
-                className="heading">
-                Hi There!{" "}
-              </h1>
-
-              <h1 className="heading-name">
-                Welcome to:
-                <strong className="main-name"> TOSter</strong>
-              </h1>
-
-              <div className="heading-navbar">
-                <Type />
-              </div>
+              md={5}
+              style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "400px" }}
+              />
             </Col>
+          }
+        </Row>
+        <button
+          className="button-container"
+          onClick={handleUpload}>
+          Get Started
+        </button>
 
-            {
-              <Col
-                md={5}
-                style={{ paddingBottom: 20 }}>
-                <img
-                  src={homeLogo}
-                  alt="home pic"
-                  className="img-fluid"
-                  style={{ maxHeight: "400px" }}
-                />
-              </Col>
-            }
-          </Row>
-        </Container>
-        <button className = 'button-container' onClick = {handleUpload}>Get Started</button>
-      
-    
-      <button className = 'button-container' onClick = {handleSearch}>Search</button>
+        <button
+          className="button-container"
+          onClick={handleSearch}>
+          Search
+        </button>
       </Container>
-      
-        
-     </section>
-
-    </>
+    </Container>
   );
-  
 }
-
 
 export default Home;
