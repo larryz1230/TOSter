f906b35e220bfaac55057d864ec2c76486d6f97f
Frank Wang <frankningwang@gmail.com>
2023-07-21 21:08:08 -0700

    larrys website tempalte

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Type from "./Type";
-import Typewriter from "typewriter-effect";
 
 function Home() {
   return (
@@ -38,18 +37,46 @@ function Home() {
     //     </Container>
     //   </Container>
     // </section>
-    <>
-      <div>Home</div>
-      <Typewriter
-        options={{
-          strings: ["TOSter", "Best Company in the wrld"],
-          autoStart: true,
-          loop: true,
-          pauseFor: 3000,
-          deleteSpeed: 100,
-        }}
-      />
-    </>
+    <section>
+      <Container
+        fluid
+        className="home-section"
+        id="home">
+        <Container className="home-content">
+          <Row>
+            <Col
+              md={7}
+              className="home-header">
+              <h1
+                style={{ paddingBottom: 15 }}
+                className="heading">
+                Hi There!{" "}
+              </h1>
+
+              <h1 className="heading-name">
+                Welcome to:
+                <strong className="main-name"> Hackathon Project</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            {/*<Col
+              md={5}
+              style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+  </Col>*/}
+          </Row>
+        </Container>
+      </Container>
+    </section>
   );
 }
 
