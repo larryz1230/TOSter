72b05d2bef761b4dc36c8d1126799853b0cfa83a
Larry Zhi <larryzhi@Larrys-MacBook-Pro.local>
2023-07-21 19:42:56 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/Home'
+import Search from './components/search/Search'
+import Upload from './components/upload/Upload'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import "./index.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-     <Home/>
-    </div>
+    <Router>
+      <div className="App" >
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/upload/Upload.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Upload() {
+    return (
+        <div>
+        Upload
+    </div>
+
+    );
+}
+
+export default Upload;
